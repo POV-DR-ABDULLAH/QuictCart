c327f9a0dd56afec6c54c369661e3cb9abdf6126
POV-DR-ABDULLAH <abdullahpondokit2024@gmail.com>
2025-05-01 08:37:59 +0800

    fix: title

--- app/layout.js
+++ app/layout.js
@@ -7,8 +7,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 
 export const metadata = {
-  title: "QuickCart - GreatStack",
-  description: "E-Commerce with Next.js ",
+  title: "ShoopQuickCart",
+  description: "E-Commerce Dengan Next.js ",
 };
 
 export default function RootLayout({ children }) {
