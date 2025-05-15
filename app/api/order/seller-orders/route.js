cb80894a90a8c754bdbcc95689b47d6d1cc8e774
POV-DR-ABDULLAH <abdullahpondokit2024@gmail.com>
2025-05-15 21:24:28 +0800

    fix: My Web

---
+++ app/api/order/seller-orders/route.js
@@ -0,0 +1,30 @@
+import { connectDB } from "@/config/db";
+import { NextResponse } from "next/server";
+import { getAuth } from "@clerk/nextjs/server";
+import { authSeller } from "@/utils/authSeller"
+import Order from "@/models/Order";
+import Address from "@/models/Address";
+
+export async function GET(request) {
+    try {
+
+        const { userId } = getAuth(request);
+
+        const isSeller = await authSeller(userId);
+        
+        if (!isSeller) {
+            return NextResponse.json({ success: false, message: "Unauthorized" });
+        }
+        
+        await connectDB();
+
+        Address.length
+
+        const orders = await Order.find({}).populate("address items.product");
+
+        return NextResponse.json({ success: true, orders });
+        
+    } catch (error) {
+        return NextResponse.json({ success: false, message: error.message });
+    }
+}
